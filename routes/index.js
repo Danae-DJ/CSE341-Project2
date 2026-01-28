d7c79ef3d144dcb6fb5f3b5cb89894b752d2d993
Danae-DJ <danae_taiyaki@hotmail.com>
2026-01-27 22:38:39 -0600

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
-router.use('/', require('./swagger'));
-
 router.get('/', (req, res) => {
-    //#swagger.tags=['Wellcome Dear Cliente']
-    res.send('Wellcome Dear Cliente');
-})
+  //#swagger.tags=['Wellcome Dear Cliente']
+  res.send('Wellcome Dear Cliente');
+});
 
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
+
+// Swagger SIEMPRE al final
+router.use('/', require('./swagger'));
 
 module.exports = router;
